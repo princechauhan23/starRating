f48550e509931364fde631aa56c30433823bc87c
Prince Chauhan <princechauhan546@gmail.com>
2022-11-26 02:59:22 +0530

    rating update

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-import {app,signInAnonymously,signOut} from "./init.js";
+import {app,signInAnonymously,signOut, fireStore,collection, addDoc, getDocs} from "./init.js";
+
+var uId = null;
+
+
+const getProductRatings = () => {
+    var ratings = {
+        sony : [],
+        samsung : [],
+        vizio: [],
+        panasonic: [],
+        phillips: []
+    }
+    return new Promise((resolve, reject) => {
+        getDocs(collection(fireStore, "ratings")).then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let data = doc.data();
+                ratings[data.productSelected].push(Number(data.ratingSelected));
+            });
+            return resolve(ratings);
+        });
+    })
+}
+const renderRatings = () => {
+    getProductRatings().then((ratings) => {
+        for (let key in ratings){
+            var total = ratings[key].reduce((acc, curr) => acc + curr,0);
+            let avg = total / ratings[key].length;
+            if (isNaN(avg)){
+                avg = 0;
+            }
+            document.getElementById(key).style.width = `${avg * 20}%`;
+            document.getElementById("num-"+key).innerHTML = `${avg.toFixed(2)}`;
+        }
+        })
+    }
+
+renderRatings();
+
+setTimeout(() => {
+    if (app.currentUser) {
+        signInBtn.innerHTML = "Log Out";
+        uId = app.currentUser.uid;
+    }
+    signInBtn.style.visibility = "visible";
+},500)
+
 
 const signInBtn = document.getElementById("sign");
 signInBtn.addEventListener("click", () => {
@@ -12,30 +58,63 @@ signInBtn.addEventListener("click", () => {
         return
     }
     signInAnonymously(app).then(data => {
-        console.log("signed in", data);
+        uId = data.user.uid;
         signInBtn.innerHTML = "Log Out";
     }).catch(err => {
         console.log("error", err);
     })
 });
 
+const submitBtn = document.getElementById("submit-btn");
+
+var productSelected;
+var ratingSelected;
+var isValid = false;
+
+const validate = () => {
+    console.log(productSelected, ratingSelected);
+    if (productSelected && ratingSelected){
+        isValid = true;
+        submitBtn.style.backgroundColor = "green";
+    }else{
+        isValid = false;
+        submitBtn.style.backgroundColor = "#ccc";
+    }
+}
+
+
+
 const form = document.getElementById("input-form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("form submitted");
+    if (!isValid) { 
+        alert("Please select a product and rating");
+        return;
+    }
+    console.log(fireStore, collection, addDoc)
+    addDoc(collection(fireStore, "ratings"),{productSelected, ratingSelected, uId}).then(() => {
+        renderRatings()
+        console.log("rating submitted");
+    }).catch((error) => {
+        console.log("error",error);
+    });
 });
 
 const selectItem = document.getElementById("select-Item");
 selectItem.addEventListener("change", (e) => {
-    console.log("item selected");
+    productSelected = e.target.value;
+    validate();
 });
 
 const ratingInput = document.getElementById("rating-input");
-ratingInput.addEventListener("change", (e) => {
-    console.log("rating input");
+ratingInput.addEventListener("input", (e) => {
+    ratingSelected = e.target.value;
+    validate();
 });
 
 const submitButton = document.getElementById("submit-btn");
 submitButton.addEventListener("click", (e) => {
     console.log("submit button clicked");
 });
+
+const starsInner = document.getElementById("stars-inner");
--- init.js
+++ init.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { signInAnonymously,signOut,getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { getFirestore,collection, addDoc,getDocs } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { signInAnonymously,signOut,getAuth} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 const firebaseConfig = {
         apiKey: "AIzaSyDGgDaT5o7vlYuT_DPRp8cwTTDFp9qViYw",
         authDomain: "productrating-e88e3.firebaseapp.com",
@@ -9,5 +10,6 @@ const firebaseConfig = {
         appId: "1:555354440326:web:147bd6524cb01b47b3c707"
 };
 let app = initializeApp(firebaseConfig);
+let fireStore = getFirestore(app);
 app = getAuth(app);
-export {app,signInAnonymously,signOut}
+export {app,signInAnonymously,signOut, fireStore,collection, addDoc,getDocs};
