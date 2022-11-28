2792dce80e7f9801fdf1b81f95f925bf5700a8f7
Prince Chauhan <princechauhan546@gmail.com>
2022-11-28 14:45:14 +0530

    env setup

--- init.js
+++ init.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getFirestore,collection, addDoc,getDocs } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import { signInAnonymously,signOut,getAuth} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import Env from "./env.js";
+
 const firebaseConfig = {
-        apiKey: "AIzaSyDGgDaT5o7vlYuT_DPRp8cwTTDFp9qViYw",
-        authDomain: "productrating-e88e3.firebaseapp.com",
-        projectId: "productrating-e88e3",
-        storageBucket: "productrating-e88e3.appspot.com",
-        messagingSenderId: "555354440326",
-        appId: "1:555354440326:web:147bd6524cb01b47b3c707"
+        apiKey: Env.apiKey,
+        authDomain: Env.authDomain,
+        projectId: Env.projectId,
+        storageBucket: Env.storageBucket,
+        messagingSenderId: Env.messagingSenderId,
+        appId: Env.appId
 };
 let app = initializeApp(firebaseConfig);
 let fireStore = getFirestore(app);
