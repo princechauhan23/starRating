0dc7f859fa1545d7f76695f959d3a4547681415c
Prince Chauhan <princechauhan546@gmail.com>
2023-04-01 02:34:27 +0530

    evn improved

---
+++ env.js
@@ -0,0 +1,10 @@
+const Env = {
+  APIKEY: "AIzaSyDGgDaT5o7vlYuT_DPRp8cwTTDFp9qViYw",
+  AUTHDOMAIN: "productrating-e88e3.firebaseapp.com",
+  PROJECTID: "productrating-e88e3",
+  STORAGEBUCKET: "productrating-e88e3.appspot.com",
+  MESSAGINGSENDERID: "555354440326",
+  APPID: "1:555354440326:web:147bd6524cb01b47b3c707",
+};
+
+export default Env;
--- init.js
+++ init.js
@@ -1,17 +1,36 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore,collection, addDoc,getDocs } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import { signInAnonymously,signOut,getAuth} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import {
+  signInAnonymously,
+  signOut,
+  getAuth,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import Env from "./env.js";
 
+console.log(Env);
+
 const firebaseConfig = {
-        apiKey: Env.apiKey,
-        authDomain: Env.authDomain,
-        projectId: Env.projectId,
-        storageBucket: Env.storageBucket,
-        messagingSenderId: Env.messagingSenderId,
-        appId: Env.appId
+  apiKey: Env.APIKEY,
+  authDomain: Env.AUTHDOMAIN,
+  projectId: Env.PROJECTID,
+  storageBucket: Env.STORAGEBUCKET,
+  messagingSenderId: Env.MESSAGINGSENDERID,
+  appId: Env.APPID,
 };
 let app = initializeApp(firebaseConfig);
 let fireStore = getFirestore(app);
 app = getAuth(app);
-export {app,signInAnonymously,signOut, fireStore,collection, addDoc,getDocs};
+export {
+  app,
+  signInAnonymously,
+  signOut,
+  fireStore,
+  collection,
+  addDoc,
+  getDocs,
+};
